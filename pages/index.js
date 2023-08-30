f7e4674eed6acc64844fada2f3dce548a6d6a063
Vivek G <audit536@gmail.com>
2023-08-30 13:44:58 +0530

    Add analytics

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,17 @@ function Home() {
     a.download = 'params.json';
   }, [parametersObject])
 
+  // analytics
+  useEffect(() => {
+    window._mfq = window._mfq || [];
+    (function () {
+      var mf = document.createElement("script");
+      mf.type = "text/javascript"; mf.defer = true;
+      mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+      document.getElementsByTagName("head")[0].appendChild(mf);
+    })();
+  })
+
 
   return (
     <div>
